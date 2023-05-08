fc67a4e3cbd6deb49ee552518bdb2c142d0f452c
Kichinda34 <leandroschneiderlima@gmail.com>
2023-05-08 16:00:22 -0300

    Adicionar arquivos de projeto.

---
+++ Entrada de Dados/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o seu nome: ");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Olá, " + nome);
+
+        Console.WriteLine("Digite sua idade: ");
+        string idade = Console.ReadLine();
+        Console.WriteLine(nome + "a sua idade é: " + idade + " anos ");
+        int idadeInt = Convert.ToInt32(idade);
+
+        Console.WriteLine("Pressione qualquer tecla: ");
+        char TeclaPressionada = Console.ReadKey(true).KeyChar;
+
+        string TeclaPressionadaString = Convert.ToString(Console.ReadKey(true).KeyChar);
+        string TeclaPressionadaString2 = Console.ReadKey(true).KeyChar.ToString();
+
+        Console.WriteLine("Olá " + nome + " a tecla que você pressionou foi: " + TeclaPressionada);
+
+        Console.ReadKey();
+    }
+}
